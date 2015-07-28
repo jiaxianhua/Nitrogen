38e8032037a03c30456a4454b2610b821c891e21
jiaxianhua <jiaxianhua@gmail.com>
2015-07-28 18:41:57 +0800

    Framework

---
+++ NDS.framework/Headers/NDS.h
@@ -0,0 +1,33 @@
+//
+//  NDS.h
+//  Nitrogen
+//
+//  Created by JiaXianhua on 15/6/12.
+//  Copyright (c) 2015年 Nitrogen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NDS.
+FOUNDATION_EXPORT double XXVersionNumber;
+
+//! Project version string for NDS.
+FOUNDATION_EXPORT const unsigned char XXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NDS/PublicHeader.h>
+
+#import <NDS/MenuCell.h>
+#import <NDS/NitrogenSideBarViewController.h>
+#import <NDS/SASlideMenuContentSegue.h>
+#import <NDS/SASlideMenuDataSource.h>
+#import <NDS/SASlideMenuDelegate.h>
+#import <NDS/SASlideMenuLeftMenuSegue.h>
+#import <NDS/SASlideMenuNavigationController.h>
+#import <NDS/SASlideMenuPushSegue.h>
+#import <NDS/SASlideMenuRightMenuSegue.h>
+#import <NDS/SASlideMenuRightMenuViewController.h>
+#import <NDS/SASlideMenuRootViewController.h>
+#import <NDS/SASlideMenuViewController.h>
+
+#import <NDS/NitrogenEmulatorViewController.h>
+#import <NDS/NitrogenGame.h>
